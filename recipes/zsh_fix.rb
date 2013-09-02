e62cf86c144302795f3b101dc22867891d6b4209
Bryan Konowitz <konowitzb@gmail.com>
2013-09-02 10:47:40 -0700

    added setting default shell to brew zsh on mac osx

--- recipes/zsh_fix.rb
+++ recipes/zsh_fix.rb
@@ -1,6 +1,12 @@
 include_recipe 'zsh'
 
 if platform?('mac_os_x')
+  execute 'Add brew ZSH to etc/shells' do
+    command 'echo "/usr/local/bin/zsh" >> /etc/shells'
+  end
+  execute 'Set brew ZSH as default' do
+    command 'chsh -s /usr/local/bin/zsh $USER'
+  end
   execute 'Fix OSX environment bug' do
     command 'mv /etc/{zshenv,zshrc}'
   end
