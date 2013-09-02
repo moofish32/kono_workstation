b45cfd2c08d123c4bc2e9867edac16e8565cc250
Bryan Konowitz <konowitzb@gmail.com>
2013-09-02 13:19:49 -0700

    WIP

--- recipes/zsh_fix.rb
+++ recipes/zsh_fix.rb
@@ -4,9 +4,16 @@ if platform?('mac_os_x')
   execute 'Add brew ZSH to etc/shells' do
     command 'echo "/usr/local/bin/zsh" >> /etc/shells'
   end
-  execute 'Set brew ZSH as default' do
-    command 'chsh -s /usr/local/bin/zsh $USER'
+  execute 'Set default zsh temporarily' do
+    command 'chsh -s /bin/zsh'
   end
+  execute 'Set default zsh for user temporarily' do
+    command 'chsh -s /bin/zsh'
+    user node['current_user']
+  end
+  # execute 'Set brew ZSH as default' do
+  #   command 'chsh -s /usr/local/bin/zsh $USER'
+  # end
   execute 'Fix OSX environment bug' do
     command 'mv /etc/{zshenv,zshrc}'
   end
