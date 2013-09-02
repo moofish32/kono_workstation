10e4c075e2ac6ea198ab9b783b1a0a7d75c33085
Bryan Konowitz <konowitzb@gmail.com>
2013-09-02 12:55:32 -0700

    WIP

--- recipes/rbenv.rb
+++ recipes/rbenv.rb
@@ -27,6 +27,12 @@ elsif platform_family?('debian')
   include_recipe "rbenv::user_install"
 end
 
-rbenv_ruby "jruby-1.7.4"
-rbenv_ruby "2.0.0-p247"
+rbenv_ruby "jruby-1.7.4" do
+  user node['current_user']
+  action :install
+end
+rbenv_ruby "2.0.0-p247" do
+  user node['current_user']
+  action :install
+end
 rbenv_global "2.0.0-p247"
