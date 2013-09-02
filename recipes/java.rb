f863efdb85858b4963f138dac0ac4e3de16f136b
Bryan Konowitz <konowitzb@gmail.com>
2013-09-02 11:51:42 -0700

    WIP

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,9 +1,8 @@
 if platform?('mac_os_x')
-  dmg_package "Java 7 Update 25 JDK" do
+  dmg_package "JDK 7 Update 25" do
     source "https://s3-us-west-1.amazonaws.com/kono-storage/public/software/jdk-7u25-macosx-x64.dmg"
     action :install
     type "pkg"
-    package_id "com.oracle.jre"
   end
 elsif platform_family?('debian')
   include_recipe "java::oracle"
