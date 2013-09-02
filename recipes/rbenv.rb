7e76009b38f4d5651f3d05d5d3648efd3fc75a0a
Bryan Konowitz <konowitzb@gmail.com>
2013-09-02 14:35:01 -0700

    WIP

--- recipes/rbenv.rb
+++ recipes/rbenv.rb
@@ -7,8 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe "applications::default"
-include_recipe "rbenv"
-include_recipe "ruby_build"
 include_recipe 'kono_workstation::java'
 
 if platform?('mac_os_x')
@@ -23,10 +21,18 @@ if platform?('mac_os_x')
   package 'rbenv-gem-rehash' do
     action [:install, :upgrade]
   end
+
+  execute 'Install rubies as user' do
+    user node['current_user']
+    command "export RBENV_ROOT=/usr/local/var/rbenv;/usr/local/bin/rbenv install jruby-1.7.4;/usr/local/bin/rbenv install 2.0.0-p247;/usr/local/bin/rbenv global 2.0.0-p247"
+  end
+
 elsif platform_family?('debian')
+  include_recipe "ruby_build"
   include_recipe "rbenv::user_install"
+
+  rbenv_ruby "jruby-1.7.4"
+  rbenv_ruby "2.0.0-p247"
+  rbenv_global "2.0.0-p247"
 end
 
-rbenv_ruby "jruby-1.7.4"
-rbenv_ruby "2.0.0-p247"
-rbenv_global "2.0.0-p247"
