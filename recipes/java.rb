11c0a18327c69ef99ddc9f62dac9532d0d7af20b
Bryan Konowitz <konowitzb@gmail.com>
2013-09-02 11:45:04 -0700

    switched to using personal s3 install of jdk7

--- recipes/java.rb
+++ recipes/java.rb
@@ -1,6 +1,6 @@
 if platform?('mac_os_x')
   dmg_package "Java 7 Update 25 JDK" do
-    source "http://download.oracle.com/otn-pub/java/jdk/7u25-b15/jdk-7u25-macosx-x64.dmg"
+    source "https://s3-us-west-1.amazonaws.com/kono-storage/public/software/jdk-7u25-macosx-x64.dmg"
     action :install
     type "pkg"
     package_id "com.oracle.jre"
