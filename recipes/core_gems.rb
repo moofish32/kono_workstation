c3d4d05447e2e8c61e5684b286e382331cb7228d
Bryan Konowitz <konowitzb@gmail.com>
2013-09-02 21:42:37 -0700

    WIP

--- recipes/core_gems.rb
+++ recipes/core_gems.rb
@@ -4,12 +4,12 @@ include_recipe 'kono_workstation::rbenv'
 if platform?('mac_os_x')
   execute 'Installing bundler, pg, rails for 2.0.0-p247' do
     user node['current_user']
-    command 'export RBENV_ROOT=/usr/local/var/rbenv;export PATH=$RBENV_ROOT/shims:$RBENV_ROOT/bin:/usr/local/bin:/usr/local/sbin:$PATH;eval "$(rbenv init -)";rbenv shell 2.0.0-p247;/usr/local/bin/gem install bundler pg rails rspec thor unicorn guard --no-document'
+    command 'export RBENV_ROOT=/usr/local/var/rbenv;export PATH=$RBENV_ROOT/shims:$RBENV_ROOT/bin:/usr/local/bin:/usr/local/sbin:$PATH;eval "$(rbenv init -)";rbenv shell 2.0.0-p247;gem install bundler pg rails rspec thor unicorn guard --no-document'
   end
 
   execute 'Installing bundler, pg, rails, puma for jruby-1.7.4' do
     user node['current_user']
-    command 'export RBENV_ROOT=/usr/local/var/rbenv;export PATH=$RBENV_ROOT/shims:$RBENV_ROOT/bin:/usr/local/bin:/usr/local/sbin:$PATH;eval "$(rbenv init -)";rbenv shell jruby-1.7.4;/usr/local/bin/gem install bundler pg rails rspec thor puma guard --no-document'
+    command 'export RBENV_ROOT=/usr/local/var/rbenv;export PATH=$RBENV_ROOT/shims:$RBENV_ROOT/bin:/usr/local/bin:/usr/local/sbin:$PATH;eval "$(rbenv init -)";rbenv shell jruby-1.7.4;gem install bundler pg rails rspec thor puma guard --no-document'
   end
 elsif platform_family?('debian')
   %w{ 2.0.0-p247 jruby-1.7.4 }.each do |ver|
