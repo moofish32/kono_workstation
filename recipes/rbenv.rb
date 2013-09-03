a839c029d2e2d4ed4013f7b6de600113bf8b5e61
Bryan Konowitz <konowitzb@gmail.com>
2013-09-02 21:39:57 -0700

    WIP

--- recipes/rbenv.rb
+++ recipes/rbenv.rb
@@ -25,10 +25,11 @@ if platform?('mac_os_x')
   execute 'Install ruby 2.0.0-p247' do
     user node['current_user']
     command "export RBENV_ROOT=/usr/local/var/rbenv;/usr/local/bin/rbenv install 2.0.0-p247;/usr/local/bin/rbenv global 2.0.0-p247"
+    not_if { File.exists? '/usr/local/var/rbenv/versions/2.0.0-p247/' }
   end
 
   execute 'Install jruby-1.7.4' do
-    command 'export RBENV_ROOT=/usr/local/var/rbenv;export PATH=$RBENV_ROOT/shims:$RBENV_ROOT/bin:/usr/local/bin:/usr/local/sbin:$PATH;eval "$(rbenv init -)";/usr/local/bin/rbenv install jruby-1.7.4'
+    command 'export RBENV_ROOT=/usr/local/var/rbenv;/usr/local/bin/rbenv install jruby-1.7.4'
     not_if { File.exists? '/usr/local/var/rbenv/versions/jruby-1.7.4/' }
   end
 
