f740128f8fabc90c73ed9d6cc02aa260214abc3e
Bryan Konowitz <konowitzb@gmail.com>
2013-09-20 01:12:42 -0700

    initial working ubuntu sublime text 2

--- recipes/sublimetext2.rb
+++ recipes/sublimetext2.rb
@@ -1,21 +1,3 @@
-def sublime_package_path
-  if platform?('mac_os_x')
-    ["#{node['etc']['passwd'][node['current_user']]['dir']}/Library/Application Support/Sublime Text 2", "Packages"]
-  elsif platform?('ubuntu')
-    ["#{node['etc']['passwd'][node['current_user']]['dir']}/.config/sublime-text-2", "Packages"]
-  end
-end
-
-spp = sublime_package_path
-
-def package_dir
-  if platform?('mac_os_x')
-    package_dir = "#{node['etc']['passwd'][node['current_user']]['dir']}/Library/Application Support/Sublime Text 2/Installed Packages"
-  elsif platform?('ubuntu')
-    package_dir = "#{node['etc']['passwd'][node['current_user']]['dir']}/.config/sublime-text-2/Installed Packages"
-  end
-end
-
 if platform?('mac_os_x')
   dmg_package "Sublime Text 2" do
     dmg_name "Sublime%20Text%202.0.1"
@@ -25,9 +7,13 @@ if platform?('mac_os_x')
   link "/usr/local/bin/subl" do
     to "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
   end
+  package_dir =["#{node['etc']['passwd'][node['current_user']]['dir']}/Library/Application Support/Sublime Text 2", "Packages"]
+  sublime_package_dir = ["#{node['etc']['passwd'][node['current_user']]['dir']}/Library/Application Support/Sublime Text 2", "Packages"]
 end
 
 if platform?('ubuntu')
+  package_dir = "#{node['etc']['passwd'][node['current_user']]['dir']}/.config/sublime-text-2/Installed Packages"
+  sublime_package_path =  ["#{node['etc']['passwd'][node['current_user']]['dir']}/.config/sublime-text-2", "Packages"]
   sublime_download = "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2"
   sublime_tar_ball = "\'Sublime Text 2.0.2 x64.tar.bz2\'"
   script "sublime text 2" do
@@ -40,6 +26,7 @@ if platform?('ubuntu')
     wget #{sublime_download}
     sudo tar -xf #{sublime_tar_ball}
     sudo mv 'Sublime Text 2' /opt
+    sudo rm Sublime*
     EOH
   end
 end
@@ -50,14 +37,13 @@ recursive_directories sublime_user_path do
   owner node['current_user']
 end
 
-# node["sublime_text_packages"].each do |sbt_package|
-#   package sbt_package["name"] do
-#     source sbt_package["source"]
-#     destination File.join(spp)
-#     owner node['current_user']
-#   end
-# end
-
+node["sublime_text_packages"].each do |package|
+  kono_workstation_sublime_package package["name"] do
+    source package["source"]
+    destination File.join(sublime_package_path)
+    owner node['current_user']
+  end
+end
 template File.expand_path("Preferences.sublime-settings", File.join(sublime_user_path)) do
   source "sublime_text-Preferences.sublime-settings.erb"
   owner node['current_user']
